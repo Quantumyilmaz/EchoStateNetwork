c4b7ecdb5a7b8e6a2e96781cea166e3192a5fbe4
Quantumyilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2021-12-23 07:12:40 +0100

    _

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Ahmet Ege Yilmaz'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # extensions = ['sphinx.ext.autosectionlabel','myst_parser']
-extensions = ['sphinx.ext.autosectionlabel']
+extensions = ['sphinx.ext.autosectionlabel','myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
